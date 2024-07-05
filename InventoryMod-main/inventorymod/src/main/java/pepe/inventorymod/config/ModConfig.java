61fdded84ec6e737212653ca4dc0e776cbaf9cc0
Pepelespooder <54486615+Pepelespooder@users.noreply.github.com>
2024-07-04 20:56:57 -0400

    Delete InventoryMod-main/inventorymod directory

--- InventoryMod-main/inventorymod/src/main/java/pepe/inventorymod/config/ModConfig.java
+++
@@ -1,9 +0,0 @@
-package pepe.inventorymod.config;
-
-import net.minecraftforge.eventbus.api.IEventBus;
-
-public class ModConfig {
-    public static void register(final IEventBus modEventBus) {
-        // Config registration code
-    }
-}
